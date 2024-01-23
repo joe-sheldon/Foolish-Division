cfdd7d8116f627f71749e1931cdc7fe1002c20db
Joseph Sheldon Jr <kb3vwt@gmail.com>
2024-01-22 20:14:48 -0700

    cors change

--- foolish_division/settings.py
+++ foolish_division/settings.py
@@ -136,5 +136,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+    "http://localhost:3000",
+    "https://www.foolish-division.com"
 ]
